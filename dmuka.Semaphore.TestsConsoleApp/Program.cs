305eb8eee676d29188734eba5661fe790ac96607
Muhammet Kandemir <k8a1n6d3e3m7i11r4@hotmail.com>
2018-10-10 20:43:51 +0300

    Complated version 1

---
+++ dmuka.Semaphore.TestsConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace dmuka.Semaphore.TestsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== Muhammet Kandemir ======");
+            Console.WriteLine("Welcome dmuka.Semaphore Tests!");
+
+            // Create instance new semphore
+            ActionQueue semaphore = new ActionQueue(4);
+            // Start semaphore as async
+            semaphore.Start();
+
+            var rowIndex = 0;
+            // This thread is for add 10 action per 5 second
+            // And each action wait 1 second after complate
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        semaphore.AddAction(() =>
+                        {
+                            Console.WriteLine("Row Index = {0}", ++rowIndex);
+                            Thread.Sleep(1000);
+                        });
+                    }
+
+                    Thread.Sleep(5000);
+                }
+            }).Start();
+
+            while (true)
+            {
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
--- dmuka.Semaphore/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace dmuka.Semaphore
-{
-    public class Class1
-    {
-    }
-}
